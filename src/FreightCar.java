86c310e9074b9717461ab146cc9900d9cd8d1af2
JoannaGoj <joannagojan@gmail.com>
2023-04-10 07:07:41 +0200

    Cleaning up car classes

--- src/FreightCar.java
+++ src/FreightCar.java
@@ -1,15 +1,12 @@
 public abstract class FreightCar extends Cars {
 
-
-    public FreightCar(String securityInformation, Integer netWeight) {
-        super(securityInformation, netWeight);
-    }
-
-    public abstract Integer getPackageWeight();
+    public abstract Integer getPackageWeight(); //child classes calculate their own package weight
 
     public abstract Integer getShipperNumber(); //how many packages are being transported
 
     public Integer getTotalWeightOfPackages() {
         return getPackageWeight() * getShipperNumber();
     }
+
+   public abstract Integer getSecurityAlertLevel(); // child classes calculate the
 }
