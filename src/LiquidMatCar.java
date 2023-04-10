e08e0f83e2ef958dc8f4469bfdef04d486ff69fd
JoannaGoj <joannagojan@gmail.com>
2023-04-10 05:32:41 +0200

    LiquidCar liquid types available

--- src/LiquidMatCar.java
+++ src/LiquidMatCar.java
@@ -1,7 +1,31 @@
 public class LiquidMatCar extends BasicFreightCar{
     private Integer basicPackageWeight = 20;
-    public LiquidMatCar(String securityInformation, Integer netWeight, Integer shipperNumber, Integer standardPackageWeight) {
+    private String[] typesOfLiquid = {"gasoline", "oil", "milk"};
+    private String liquidType;
+    //is flammable
+
+
+    public LiquidMatCar(String securityInformation, Integer netWeight,
+                        Integer shipperNumber, Integer standardPackageWeight,
+                        Integer basicPackageWeight, String liquidType) {
         super(securityInformation, netWeight, shipperNumber, standardPackageWeight);
+        this.basicPackageWeight = basicPackageWeight;
+        if (availableLiquidType(liquidType)) {
+            this.liquidType = liquidType;
+        } else {
+            throw new IllegalArgumentException("Invalid fur color: " + liquidType);
+        }
+    }
+
+
+    private boolean availableLiquidType(String liquidType) {
+        String[] availableLiquidTypes = {"gasoline", "oil", "milk"};
+        for (String color : availableLiquidTypes) {
+            if (color.equals(liquidType)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
