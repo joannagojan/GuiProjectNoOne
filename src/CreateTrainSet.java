887e2e045351d681e9e912a067509882a5478d60
JoannaGoj <joannagojan@gmail.com>
2023-04-11 09:47:54 +0200

    Trainset Generate

---
+++ src/CreateTrainSet.java
@@ -0,0 +1,49 @@
+import java.util.List;
+import java.util.Random;
+
+public class CreateTrainSet {
+
+    private Locomotive randomLocomotive;
+
+    // Randomly choosing a locomotive for a trainset
+    private Locomotive addRandomLocomotive(List<Locomotive> allLocomotives) {
+        Random random = new Random();
+        randomLocomotive = allLocomotives.get(random.nextInt(allLocomotives.size()));
+        return randomLocomotive;
+    }
+
+    // Method to get the chosen locomotive's Max Load Weight
+    private Integer getLocoMaxLoadWeight(Locomotive locomotive) {
+        return locomotive.getMaxLocoLoadWeight();
+    }
+
+    // Method to get the chosen locomotive's Maximum Cars that can be attached
+    private Integer getLocoMaxCarNumber(Locomotive locomotive) {
+        return locomotive.getMaxNumberCars();
+    }
+
+    // Adding cars to a trainset based on chosen locomotive's limits
+    private List<Cars> addRandomCars(List<Cars> allCars) {
+        Integer maxLoadInTrainset = getLocoMaxLoadWeight(randomLocomotive);
+        Integer maxNumCarsInTrainset = getLocoMaxCarNumber(randomLocomotive);
+        List<Cars> carsInTrainSet;
+
+        // assigning
+        while (maxLoadInTrainset > 0 && carsInTrainSet.size() < maxNumCarsInTrainset) {
+            Cars randomCar = allCars.get((int) (Math.random() * allCars.size()));
+            if (getLocoMaxLoadWeight(randomLocomotive) >= randomCar.getGrossWeight()) {
+                carsInTrainSet.add(randomCar);
+                maxLoadInTrainset -= randomCar.getGrossWeight();
+            }
+        }
+
+        return carsInTrainSet;
+    }
+
+    public void createTrainSetRandomly(List<Locomotive> allLocomotives, List<Cars> allCars) {
+        addRandomLocomotive(allLocomotives);
+        addRandomCars(allCars);
+    }
+
+
+}
--- src/TrainSet.java
+++ src/TrainSet.java
@@ -5,41 +5,8 @@ public class TrainSet {
     private Locomotive randomLocomotive;
     private List<Cars> carsInTrainSet;
 
-
-    // Randomly choosing a locomotive for a trainset
-    private Locomotive addRandomLocomotive(List<Locomotive> allLocomotives) {
-        randomLocomotive = allLocomotives.get((int) (Math.random() * allLocomotives.size()));
-        return randomLocomotive;
+    public TrainSet(Locomotive randomLocomotive, List<Cars> carsInTrainSet) {
+        this.randomLocomotive = randomLocomotive;
+        this.carsInTrainSet = carsInTrainSet;
     }
-
-    // Method to get the chosen locomotive's Max Load Weight
-    private Integer getLocoMaxLoadWeight(Locomotive locomotive) {
-        return locomotive.getMaxLocoLoadWeight();
-    }
-
-    // Method to get the chosen locomotive's Maximum Cars that can be attached
-    private Integer getLocoMaxCarNumber(Locomotive locomotive) {
-        return locomotive.getMaxNumberCars();
-    }
-
-// Adding cars to a trainset based on chosen locomotive's limits
-    private void addRandomCars(List<Cars> allCars) {
-        Integer maxLoadInTrainset = getLocoMaxLoadWeight(randomLocomotive);
-        Integer maxNumCarsInTrainset = getLocoMaxCarNumber(randomLocomotive);
-
-        // assigning
-        while (maxLoadInTrainset > 0 && carsInTrainSet.size() < maxNumCarsInTrainset) {
-            Cars randomCar = allCars.get((int) (Math.random() * allCars.size()));
-            if (getLocoMaxLoadWeight(randomLocomotive) >= randomCar.getGrossWeight()) {
-                carsInTrainSet.add(randomCar);
-                maxLoadInTrainset -= randomCar.getGrossWeight();
-            }
-        }
-    }
-
-    public void createTrainSetRandomly(List<Locomotive> allLocomotives, List<Cars> allCars) {
-        addRandomLocomotive(allLocomotives);
-        addRandomCars(allCars);
-    }
-
 }
