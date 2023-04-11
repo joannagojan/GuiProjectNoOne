a329568e704098cd917bb4c32d300553fd4089bb
JoannaGoj <joannagojan@gmail.com>
2023-04-11 06:15:25 +0200

    Trainsets

--- src/Locomotive.java
+++ src/Locomotive.java
@@ -4,18 +4,20 @@ public class Locomotive {
     private String locomotiveName;
     private Station locoHomeStation;
     private Station locoDestinationStation;
-    private Integer maxNumberCars;
-    private Integer maxLoadWeight;
+    private Integer maxNumberCars; // maximum number of cars that can be connected to a locomotive object
+    private Integer maxLocoLoadWeight; // maximum load a locomotive object can carry
+    private Integer locomotiveSpeed; // assigned speed to a locomotive object
+
     private static Integer locomotiveIDCounter = 0;
-    private Integer locomotiveSpeed;
+
 
 
     //Construtor of Locomotive class
-    public Locomotive(String locomotiveName, String locoHomeStation, String locoDestinationStation,
-                      String locomotiveSource, Integer maxNumberCars, Integer maxLoadWeight,
+    public Locomotive(String locomotiveName, Station locoHomeStation, Station locoDestinationStation,
+                      Integer maxNumberCars, Integer maxLocoLoadWeight,
                       Integer locomotiveSpeed) {
         // max weight of the load cannot be smaller than 5000 tons and bigger than 13000 tons
-        if (maxLoadWeight < 5000 || maxLoadWeight < 13000) {
+        if (maxLocoLoadWeight < 5000 || maxLocoLoadWeight < 13000) {
             throw new IllegalArgumentException("Maximum load weight must be in range of 5000-13000 tons ");
         }
 
@@ -24,7 +26,7 @@ public class Locomotive {
         this.locoHomeStation = locoHomeStation;
         this.locoDestinationStation = locoDestinationStation;
         this.maxNumberCars = maxNumberCars;
-        this.maxLoadWeight = maxLoadWeight;
+        this.maxLocoLoadWeight = maxLocoLoadWeight;
         this.locomotiveSpeed = locomotiveSpeed;
 
     }
@@ -35,7 +37,7 @@ public class Locomotive {
         return "This is locomotive number " + locomotiveID + " called " + locomotiveName + " it departs from " + locoHomeStation
                 + "with a destination in " + locoDestinationStation +
                 " with maximum number of cars of " + maxNumberCars + " and maximum weight load "
-                + maxLoadWeight + " of the speef of "
+                + maxLocoLoadWeight + " of the speef of "
                 + locomotiveSpeed  + '.';
     }
 }
--- src/TrainSet.java
+++ src/TrainSet.java
@@ -1,2 +1,29 @@
+import java.util.List;
+
 public class TrainSet {
+    private List<Cars> cars;
+    private Locomotive locomotive;
+
+    public void addRandomLocomotive(List<Locomotive> allLocomotives) {
+        locomotive = allLocomotives.get((int) (Math.random() * allLocomotives.size()));
+    }
+
+    // create a list of locomotives and railroad cars
+//    List<Locomotive> allLocomotives = new ArrayList<>();
+//allLocomotives.add(new Locomotive("ABC123", 1000));
+//
+//    List<RailroadCar> allCars = new ArrayList<>();
+//allCars.add(new Boxcar(500));
+//allCars.add(new TankCar(750));
+//allCars.add(new Flatcar(1000));
+//
+//    // create a new trainset and add a random locomotive and random cars
+// Trainset myTrainset = new Trainset();
+//myTrainset.addRandomLocomotive(allLocomotives);
+//myTrainset.addRandomCars(allCars, 5);
+//
+//// print the trainset
+//myTrainset.printTrainset();
+
+
 }
