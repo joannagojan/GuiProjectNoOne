794739d8f99e855377da77bfbceb97809cf45174
JoannaGoj <joannagojan@gmail.com>
2023-04-20 09:58:04 +0200

    Passenger car

--- src/Railway/CarTypes/PassengerCar.java
+++ src/Railway/CarTypes/PassengerCar.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class PassengerCar extends Cars {
+    private static final AtomicInteger passsengerCarIDCounter = new AtomicInteger(0);
+    private Integer passsengerCarID;
     private Integer numberOfSeats; // how many seats are available
     private Integer currentNumberOfPassengers;
     private ArrayList<Passenger> allPassengers;
@@ -15,6 +17,7 @@ public class PassengerCar extends Cars {
     //Constructor
 
     public PassengerCar(Integer numberOfSeats) {
+        this.passsengerCarID = passsengerCarIDCounter.incrementAndGet();
         //ranges to pick from
         if (numberOfSeats > 100 || numberOfSeats < 10) {
             throw new IllegalArgumentException("Pick a number of seats from a range 10-100, you picked: "+ numberOfSeats);
@@ -48,6 +51,35 @@ public void removePassenger(Passenger passenger) throws Exception{
         return result;
     }
 
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("PassengerCar of ")
+                .append("ID: ").append(passengerCarID)
+                .append(", total number of seats on this car: ").append(numberOfSeats)
+                .append(", current number of passengers on this car: ").append(currentNumberOfPassengers)
+                .append(", passengers on this car's IDs list: ");
+        for (Passenger passenger : allPassengers) {
+            sb.append(passenger.getPassengerID()).append(", ");
+        }
+        return sb.toString();
+    }
+
+    public Integer getPasssengerCarID() {
+        return passsengerCarID;
+    }
+
+    public Integer getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public Integer getCurrentNumberOfPassengers() {
+        return currentNumberOfPassengers;
+    }
+
+    public ArrayList<Passenger> getAllPassengers() {
+        return allPassengers;
+    }
 
     @Override
     public boolean requiredElectricity() {
